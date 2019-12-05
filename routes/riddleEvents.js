f92b563310d791d834d4048a7c69d1d2cc5c6746
Jeffrey Pluntke <jeffrey.pluntke@student.uni-luebeck.de>
2019-12-05 13:44:11 +0100

    Refactor POST /riddleEvents/:riddleID

--- routes/riddleEvents.js
+++ routes/riddleEvents.js
@@ -53,31 +53,46 @@ router.route('/').get((req, res) => {
  * @apiSuccessExample Success-Response
  * HTTP/1.1 201 OK {"id": "30"}
  */
-router.route('/:riddleID').post(({body: {data, type}, params: {riddleID}}, res) => {
+router.route('/:riddleID').post(async ({body: {data, type}, params: {riddleID}}, res) => {
   // write into db
-  global.pool.query(
-    'INSERT INTO RiddleEvent (RiddleID, Type, Data, Timestamp) VALUES ($1, $2, $3, current_timestamp) RETURNING RiddleEventID',
-    [parseInt(riddleID), type, data],
-    (err, result) => {
-      if (err) {
-        throw err;
-      } else {
-        // get riddle group and send response
-        global.pool.query(
-          'SELECT Description FROM Riddle WHERE RiddleID = $1',
-          [parseInt(riddleID)],
-          (err, {rows}) => {
-            if (err) {
-              throw err;
-            } else {
-              global.io.emit('newLogEntry', {groupID: rows[0].description, data, type});
-              return res.status(201).send({riddleeventid: result.rows[0].riddleeventid});
-            }
-          },
-        );
-      }
-    },
-  );
+  let insertResult;
+  let newTimestamp;
+  let newRiddleEventGroup;
+  // insert the new event into the db
+  try {
+    insertResult = await global.pool.query(
+      'INSERT INTO RiddleEvent (RiddleID, Type, Data, Timestamp) VALUES ($1, $2, $3, current_timestamp) RETURNING RiddleEventID',
+      [parseInt(riddleID), type, data],
+    );
+  } catch (err) {
+    throw err;
+  }
+  // get the timestamp of the new event
+  try {
+    newTimestamp = await global.pool.query(
+      'SELECT timestamp FROM RiddleEvent WHERE riddleeventid = $1',
+      [insertResult.rows[0].riddleeventid],
+    );
+  } catch (err) {
+    throw err;
+  }
+  // get the name of the riddleGroup
+  try {
+    newRiddleEventGroup = await global.pool.query('SELECT Name FROM Riddle WHERE RiddleID = $1', [
+      parseInt(riddleID),
+    ]);
+  } catch (err) {
+    throw err;
+  }
+  // emit data to the socket
+  global.io.emit('newLogEntry', {
+    groupID: newRiddleEventGroup.rows[0].name,
+    data,
+    type,
+    timestamp: newTimestamp.rows[0].timestamp,
+  });
+  // return response with the riddleventid
+  return res.status(201).send({riddleeventid: insertResult.rows[0].riddleeventid});
 });
 
 module.exports = router;
