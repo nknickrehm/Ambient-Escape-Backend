bfd30b6bf0d3b5ae458b82736fedbba2c9cea957
Jeffrey Pluntke <jeffrey.pluntke@student.uni-luebeck.de>
2019-12-06 13:34:32 +0100

    Add PATCH Route for updating the progress of a riddle

--- routes/riddles.js
+++ routes/riddles.js
@@ -1,5 +1,4 @@
-
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 /* GET REQUESTS */
@@ -31,13 +30,13 @@ const router = express.Router();
     }
 ]
  */
-router.route("/").get((req, res) => {
-    global.pool.query("SELECT * FROM Riddle", (error, results) => {
-        if (error) {
-            throw error;
-        }
-        res.status(200).json(results.rows);
-    });
+router.route('/').get((req, res) => {
+  global.pool.query('SELECT * FROM Riddle', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
 });
 
 /**
@@ -64,19 +63,22 @@ router.route("/").get((req, res) => {
     }
 ]
  */
-router.route("/names/:name").get((req, res) => {
-    global.pool.query("SELECT MAX(RiddleId) as RiddleId FROM Riddle WHERE Name=$1", [req.params['name']], (error, results) => {
-        if (error) {
-            throw error;
-        }
-        if (results.rows[0].riddleid === null) {
-            res.status(404).send();
-            return;
-        }
-        res.status(200).json(results.rows);
-    });
+router.route('/names/:name').get((req, res) => {
+  global.pool.query(
+    'SELECT MAX(RiddleId) as RiddleId FROM Riddle WHERE Name=$1',
+    [req.params['name']],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      if (results.rows[0].riddleid === null) {
+        res.status(404).send();
+        return;
+      }
+      res.status(200).json(results.rows);
+    },
+  );
 });
-
 
 /**
  * @api {get} /riddles/:riddleid/Codes Get all codes affecting the given riddle
@@ -110,14 +112,18 @@ router.route("/names/:name").get((req, res) => {
     },
 ]
  */
-router.route("/:riddleid/Codes").get((req, res) => {
-    global.pool.query("SELECT * FROM Code WHERE ReceiverRiddleID=$1", [req.params['riddleid']], (error, results) => {
-        if (error) {
-            throw error;
-        }
+router.route('/:riddleid/Codes').get((req, res) => {
+  global.pool.query(
+    'SELECT * FROM Code WHERE ReceiverRiddleID=$1',
+    [req.params['riddleid']],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
 
-        res.status(200).json(results.rows);
-    });
+      res.status(200).json(results.rows);
+    },
+  );
 });
 
 /* POST REQUESTS */
@@ -147,18 +153,35 @@ router.route("/:riddleid/Codes").get((req, res) => {
     "status": "READY"
 }
  */
-router.route("/:riddleid/Status").patch((req, res) => {
-    global.pool.query("UPDATE Riddle SET Status=$1 WHERE RiddleId=$2", [req.body.status, req.params['riddleid']], 
-            (error, results) => {
-        if (error) {
-            throw error;
+router.route('/:riddleid/Status').patch((req, res) => {
+  global.pool.query(
+    'UPDATE Riddle SET Status=$1 WHERE RiddleId=$2',
+    [req.body.status, req.params['riddleid']],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
 
-        }
-
-        //TODO: Check ready state conditions (e.g. are all riddles completed?)
-        global.io.emit("riddles", {"type": "changed", "id":req.params['riddleid']});
-        res.status(201).json(req.body);
-    });
+      //TODO: Check ready state conditions (e.g. are all riddles completed?)
+      global.io.emit('riddles', {type: 'changed', id: req.params['riddleid']});
+      res.status(201).json(req.body);
+    },
+  );
 });
- 
+
+router
+  .route('/:riddleid/:gameid/progress/:progress?')
+  .patch(async ({params: {riddleid, gameid}, query: {progress}}, res) => {
+    const updatedRiddle = await global.pool.query(
+      'UPDATE Riddle SET progress=$1 WHERE RiddleId=$2 AND gameid=$3',
+      [progress, riddleid, gameid],
+    );
+    const updatedGroup = await global.pool.query(
+      'SELECT name, progress FROM Riddle WHERE RiddleId=$1 AND GameId=$2',
+      [riddleid, gameid],
+    );
+    global.io.emit('updateRiddleProgress', updatedGroup.rows[0]);
+    return res.status(200).end();
+  });
+
 module.exports = router;
